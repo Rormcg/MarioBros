bfa2edc9d1a5146f299b4dbab5f2457293e38e54
Rory McGuire <rory.mcguire25@bishopireton.org>
2022-01-22 13:23:08 -0500

    Updated Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 
 
 public class Main {
+   
+   
+   Main() {
+      System.out.println("Hello");
+   }
+}
 
+class RunMain{
+   public static void main(String[] args) {
+      Main main = new Main();
+   }
 }
--- src/Utility.java
+++ src/Utility.java
@@ -1,5 +1,8 @@
 
 
 public class Utility {
-
+   
+   
+   
+   
 }
