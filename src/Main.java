34f4ff6cc5ed9ef1694a82ce185da5652a6a0825
Rory McGuire <rory.mcguire25@bishopireton.org>
2022-01-22 14:03:06 -0500

    Updated main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 
 
-<<<<<<< HEAD
 import javax.swing.JFrame;
 import javax.swing.JComponent;
 import java.awt.Container;
@@ -8,21 +7,21 @@ import java.awt.Graphics;
 import java.awt.Color;
 import javax.swing.Timer;
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Main extends JComponent{
+public class Main extends JComponent implements ActionListener {
    private JFrame frame;
    private Container content;
    private int screenWidth = 600, screenHeight = 600;
+   private Timer t;
    
-=======
-public class Main {
->>>>>>> 1dd60cc1fbb27bc932c50e6907aee4379a1ac984
    Main() {
       frame = new JFrame("Super Mario Bros.");
       content = frame.getContentPane();
       content.add(this);
       content.setBackground(new Color(113, 255, 255));
       setUp();
+      t = new Timer(1, this);
       
    }
    
@@ -36,6 +35,10 @@ public class Main {
    
    public void paintComponent(Graphics g) {
       g.setColor(Color.BLUE);
+   }
+   
+   public void actionPerformed(ActionEvent e) {
+   
    }
 }
 
