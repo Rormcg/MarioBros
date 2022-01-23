ee263dca0cb9ad0249a6b19dbfb3b3161601538f
Rory McGuire <rory.mcguire25@bishopireton.org>
2022-01-23 10:17:27 -0500

    Completed the game

--- src/GoalFlag.java
+++ src/GoalFlag.java
@@ -10,12 +10,16 @@ public class GoalFlag {
    
    public void draw(Graphics g) {
       g.setColor(new Color(108, 227, 52));
-      g.fillRect((int)xPos, 150, 10, 600);
+      g.fillRect((int)xPos, 130, 10, 600);
       g.setColor(new Color(39, 164, 21));
-      g.fillOval((int)(xPos - 10), 145, 30, 30);
+      g.fillOval((int)(xPos - 10), 115, 30, 30);
    }
    
    public double getXPos() {
       return xPos;
+   }
+   
+   public void setXPos(double a) {
+      xPos = a;
    }
 }
